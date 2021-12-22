8593250f47e0ed61bb939c9bd31d85107b3b39cf
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2021-12-22 10:36:50 +0200

    fix

--- src/components/TransactionForm/CustomStyles/customStyles.js
+++ src/components/TransactionForm/CustomStyles/customStyles.js
@@ -6,7 +6,7 @@ const customStyles = {
       state.isSelected || state.isFocused ? '#FFFFFF' : 'inherit',
     paddingLeft: 20,
     paddingTop: 10,
-    height: 34,
+    height: 44,
     border: 'none',
     cursor: 'pointer',
   }),
@@ -34,6 +34,7 @@ const customStyles = {
     margin: 0,
     height: 24,
     fontFamily: 'Circe, sans-serif',
+    color: '#000000',
     fontSize: 18,
     lineHeight: 1.5,
   }),
@@ -42,7 +43,6 @@ const customStyles = {
     ...provided,
     margin: 0,
     padding: 0,
-    color: '#000000',
   }),
 
   indicatorsContainer: () => ({
@@ -55,6 +55,7 @@ const customStyles = {
     borderRadius: 20,
     boxShadow: 'none',
   }),
+
   placeholder: provided => ({
     ...provided,
     margin: 0,
@@ -64,6 +65,7 @@ const customStyles = {
     lineHeight: 1.5,
     fontWeight: 400,
   }),
+
   menuList: provided => ({
     ...provided,
     '@media screen and (min-width: 320px)': {
@@ -76,9 +78,40 @@ const customStyles = {
     backdropFilter: 'blur(50px)',
     borderRadius: 20,
     boxShadow: '0px 6px 15px rgba(0, 0, 0, 0.1)',
-    // fontSize: 18,
-    // lineHeight: 1.5,
+    fontSize: 18,
+    lineHeight: 1.5,
+    fontWeight: 400,
+
+    '::-webkit-scrollbar': {
+      height: '100%',
+      width: 5,
+      backgroundColor: 'transparent',
+    },
+
+    '::-webkit-scrollbar-track-piece:corner-present': {
+      marginTop: 12,
+    },
+    '::-webkit-scrollbar-track-piece:start': {
+      background: 'transparent',
+      marginTop: 10,
+      overflow: 'hidden',
+    },
+
+    '::-webkit-scrollbar-track-piece:end': {
+      backgroundColor: 'transparent',
+      marginBottom: 10,
+    },
+    '::-webkit-scrollbar-thumb': {
+      borderRadius: 10,
+      //   backgroundColor: '#FF6596',
+      backgroundColor: '#24CCA7',
+    },
+    '::-webkit-scrollbar-track': {
+      borderRadius: 20,
+      backgroundColor: 'transparent',
+    },
   }),
+
   container: provided => ({
     ...provided,
     borderBottom: '1px solid #E0E0E0',
