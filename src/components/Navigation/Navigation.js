da13b875365b48449b868ce4d0ce05efb12611a8
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2021-12-24 18:37:19 +0200

    changes navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,8 @@
 import { NavLink } from 'react-router-dom';
 import Media from 'react-media';
-import { Home } from '../IconBtn/Home';
-import { Statistic } from '../IconBtn/Statistic';
-import { Currenc } from '../IconBtn/Currenc';
+import { HomeIcon } from '../IconBtn/HomeIcon';
+import { StatisticsIcon } from '../IconBtn/StatisticsIcon';
+import { CurrencyIcon } from '../IconBtn/CurrencyIcon';
 
 import styles from './Navigation.module.scss';
 
@@ -10,47 +10,55 @@ function Navigation() {
   return (
     <div>
       <nav className={styles.navigation}>
-        <NavLink
-          to="home"
-          className={({ isActive }) => {
-            return isActive ? styles.navLinkActive : styles.navLink;
-          }}
-        >
-          <div className={styles.boxNav}>
-            <Home svg={styles.svgNav} />
-            <span className={styles.textNav}>Home</span>
-          </div>
-        </NavLink>
-        <NavLink
-          to="statistics"
-          className={({ isActive }) => {
-            return isActive ? styles.navLinkActive : styles.navLink;
-          }}
-        >
-          <div className={styles.boxNav}>
-            <Statistic svg={styles.svgNav} />
-            <span className={styles.textNav}>Statistics</span>
-          </div>
-        </NavLink>
-        <Media
-          queries={{
-            mobileSize: '(max-width: 767px)',
-            otherSize: '(min-width: 768px)',
-          }}
-        >
-          {matches =>
-            matches.mobileSize && (
-              <NavLink
-                to="currency"
-                className={({ isActive }) => {
-                  return isActive ? styles.navLinkActive : styles.navLink;
-                }}
-              >
-                <Currenc svg={styles.svgNav} />
-              </NavLink>
-            )
-          }
-        </Media>
+        <ul>
+          <li>
+            <NavLink
+              to="home"
+              className={({ isActive }) => {
+                return isActive ? styles.navLinkActive : styles.navLink;
+              }}
+            >
+              <div className={styles.boxNav}>
+                <HomeIcon svg={styles.svgNav} />
+                <span className={styles.textNav}>Home</span>
+              </div>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="statistics"
+              className={({ isActive }) => {
+                return isActive ? styles.navLinkActive : styles.navLink;
+              }}
+            >
+              <div className={styles.boxNav}>
+                <StatisticsIcon svg={styles.svgNav} />
+                <span className={styles.textNav}>Statistics</span>
+              </div>
+            </NavLink>
+          </li>
+          <Media
+            queries={{
+              mobileSize: '(max-width: 767px)',
+              otherSize: '(min-width: 768px)',
+            }}
+          >
+            {matches =>
+              matches.mobileSize && (
+                <li>
+                  <NavLink
+                    to="currency"
+                    className={({ isActive }) => {
+                      return isActive ? styles.navLinkActive : styles.navLink;
+                    }}
+                  >
+                    <CurrencyIcon svg={styles.svgNav} />
+                  </NavLink>
+                </li>
+              )
+            }
+          </Media>
+        </ul>
       </nav>
     </div>
   );
